ac62069afbd3127d944d415689effe8793b799f0
Jacob Feder <jacobsfeder@gmail.com>
2018-06-29 00:15:02 -0400

    -

--- axis_fifo_test/settings.sh
+++ axis_fifo_test/settings.sh
@@ -7,5 +7,7 @@ PETALINUX_INSTALL_DIR=/opt/Xilinx/Petalinux/2018.1
 # additional arguments to pass to vivado generate.tcl script
 #VIVADO_GENERATE_ARGS=arg1 arg2
 
+DEFAULT_PROJECT_NAME=default
+
 # run 'get_board_parts' tcl command in Vivado for available boards
 BOARD_NAME=digilentinc.com:arty-z7-20:part0:1.0
--- axis_fifo_test/src/linux/copy.sh
+++ axis_fifo_test/src/linux/copy.sh
@@ -1,12 +1,15 @@
 SOURCE_DIR=$1
 LINUX_PROJECT_PATH=$2
 
+# Arty Z7 board requires this for USB functionality
+cp -f -v $SOURCE_DIR"/system-user.dtsi" $LINUX_PROJECT_PATH"/project-spec/meta-user/recipes-bsp/device-tree/files/system-user.dtsi"
+
 # startup script
-cp -f -v $SOURCE_DIR"/startup/startup" $LINUX_PROJECT_PATH"/recipes-apps/startup/files/startup"
-cp -f -v $SOURCE_DIR"/startup/startup.bb" $LINUX_PROJECT_PATH"/recipes-apps/startup/startup.bb"
+cp -f -v $SOURCE_DIR"/startup/startup" $LINUX_PROJECT_PATH"/project-spec/meta-user/recipes-apps/startup/files/startup"
+cp -f -v $SOURCE_DIR"/startup/startup.bb" $LINUX_PROJECT_PATH"/project-spec/meta-user/recipes-apps/startup/startup.bb"
 
 # axis-fifo driver
-cp -f -v $SOURCE_DIR"/axis-fifo/axis-fifo.c" $LINUX_PROJECT_PATH"/recipes-modules/axis-fifo/files/axis-fifo.c"
+cp -f -v $SOURCE_DIR"/axis-fifo/axis-fifo.c" $LINUX_PROJECT_PATH"/project-spec/meta-user/recipes-modules/axis-fifo/files/axis-fifo.c"
 
 # fifo test program
-cp -f -v $SOURCE_DIR"/fifo-test/fifo-test.c" $LINUX_PROJECT_PATH"/recipes-apps/fifo-test/files/fifo-test.c"
+cp -f -v $SOURCE_DIR"/fifo-test/fifo-test.c" $LINUX_PROJECT_PATH"/project-spec/meta-user/recipes-apps/fifo-test/files/fifo-test.c"
